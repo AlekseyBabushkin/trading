d99fd6ee23ffc8c66d12875a81d83a9086f158b3
AlekseyBabushkin <babushkin058@mail.ru>
2022-06-06 23:03:03 +0300

    header + hero

--- src/common.blocks/burger/burger.js
+++ src/common.blocks/burger/burger.js
@@ -1,19 +1,26 @@
 /* eslint-disable prefer-destructuring */
 const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav');
+const nav = document.querySelector('.header__nav');
+const close = document.querySelector('.header__close');
 const navItem = document.querySelectorAll('.nav__link');
 const body = document.body;
 
 burger.addEventListener('click', () => {
   body.classList.toggle('none-scroll');
   burger.classList.toggle('burger--active');
-  nav.classList.toggle('nav--visible');
+  nav.classList.toggle('header__nav--visible');
+});
+
+close.addEventListener('click', () => {
+  body.classList.toggle('none-scroll');
+  burger.classList.toggle('burger--active');
+  nav.classList.toggle('header__nav--visible');
 });
 
 navItem.forEach((el) => {
   el.addEventListener('click', () => {
     body.classList.remove('none-scroll');
     burger.classList.remove('burger--active');
-    nav.classList.remove('nav--visible');
+    nav.classList.remove('header__nav--visible');
   });
 });
