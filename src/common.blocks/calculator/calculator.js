a0624e9a963a02e2a78290c917d9edd58b0e8cbc
Aleksey <babushkin058@mail.ru>
2022-06-14 18:02:56 +0300

    доделки

--- src/common.blocks/calculator/calculator.js
+++ src/common.blocks/calculator/calculator.js
@@ -1,11 +1,9 @@
 const radioDate = document.querySelectorAll('.calculator__date');
 const dateOut = document.querySelector('#date-out');
 
-document.querySelector('.income__btn').onclick = () => {
-  for (let i = 0; i < radioDate.length; i++) {
-    if (radioDate[i].checked) {
-      console.log(radioDate[i].value);
-      dateOut.innerHTML = radioDate[i].value;
-    }
+for (let i = 0; i < radioDate.length; i++) {
+  if (radioDate[i].checked) {
+    console.log(radioDate[i].value);
+    dateOut.innerHTML = radioDate[i].value;
   }
-};
+}
--- src/common.blocks/header/header.js
+++ src/common.blocks/header/header.js
@@ -1,16 +1,21 @@
 const header = document.querySelector('.header');
-const hero = document.querySelector('.hero');
 const headerHeight = header.offsetHeight;
-const heroHeight = hero.offsetHeight;
+const heightScreen = window.innerHeight;
 
 window.addEventListener('scroll', () => {
   const scrollDistance = window.scrollY;
-
-  if (scrollDistance >= heroHeight + headerHeight) {
+  if (scrollDistance > headerHeight * 2) {
+    header.classList.add('header--hidden');
+  } else {
+    header.classList.remove('header--hidden');
+  }
+  // eslint-disable-next-line no-mixed-operators
+  if (scrollDistance > heightScreen - headerHeight * 1.5) {
     header.classList.add('header--fixed');
-    hero.style.marginTop = `${headerHeight}px`;
   } else {
     header.classList.remove('header--fixed');
-    hero.style.marginTop = null;
   }
+  console.log(scrollDistance);
 });
+
+console.log(heightScreen);
