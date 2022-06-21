e1abb386dec9e6bd9fba2f83464a55342e77e627
Aleksey <babushkin058@mail.ru>
2022-06-21 16:46:08 +0300

    фав иконки + доделки

--- src/common.blocks/swiper/swiper.js
+++ src/common.blocks/swiper/swiper.js
@@ -2,26 +2,28 @@ if (document.querySelector('.js-swiper') !== null) {
   document.querySelectorAll('.js-swiper').forEach((n) => {
     // eslint-disable-next-line no-new
     new Swiper(n.querySelector('.js-swiper-container'), {
-      grid: {
-        fill: 'column',
-        columns: 2,
-      },
+      slidesPerView: 1,
+      spaceBetween: 15,
+      slidesPerColumn: 3,
+      slidesPerColumnFill: 'row',
       breakpoints: {
         576: {
-          slidesPerView: 1,
+          slidesPerView: 1.5,
           spaceBetween: 15,
-          // slidesPerColumn: 3,
-          // slidesPerColumnFill: 'column',
+          slidesPerColumn: 2,
+          slidesPerColumnFill: 'row',
         },
         768: {
-          slidesPerView: 1,
+          slidesPerView: 3,
           spaceBetween: 15,
-          // slidesPerColumn: 3,
-          // slidesPerColumnFill: 'column',
+          slidesPerColumn: 1,
+          slidesPerColumnFill: 'row',
         },
         992: {
           slidesPerView: 4,
-          spaceBetween: 32,
+          spaceBetween: 26,
+          slidesPerColumn: 1,
+          slidesPerColumnFill: 'row',
         },
       },
       navigation: {
